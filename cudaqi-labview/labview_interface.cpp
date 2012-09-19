310d172221404a90353368bbc0182a1eba419290
jcnossen <j.cnossen@gmail.com>
2012-09-19 17:35:41 +0200

    * getting labview images into C++ * GPUImage class

--- cudaqi-labview/labview_interface.cpp
+++ cudaqi-labview/labview_interface.cpp
@@ -13,6 +13,8 @@ struct Position
 	float x,y,z;
 };
 
+
+
 std::string SPrintf(const char *fmt, ...) {
 	va_list ap;
 	va_start(ap, fmt);
@@ -26,14 +28,52 @@ std::string SPrintf(const char *fmt, ...) {
 
 
 
-DLL_EXPORT void CALLCONV process_image(int32_t *position, uintptr_t *imagePtr)
+DLL_EXPORT uintptr_t CALLCONV copy_image(uintptr_t srcPtr)
 {
-	ImageInfo info;
-	Image* image = (Image*)imagePtr;
+	ImageInfo info, dstInfo;
+	Image* image = (Image*)srcPtr;
 	imaqGetImageInfo(image, &info);
 
-	*position = info.xRes;
+	Image* output = imaqCreateImage(IMAQ_IMAGE_U8, 0);
+	imaqSetImageSize(output, info.xRes, info.yRes);
+	imaqGetImageInfo(output, &dstInfo);
+
+	uint8_t *src = (uint8_t*)imaqGetPixelAddress(image, Point());
+	uint8_t *dst = (uint8_t*)imaqGetPixelAddress(output, Point());
+	for (int y=0;y<info.yRes;y++) {
+		src += info.pixelsPerLine;
+		dst += info.pixelsPerLine;
+
+		for (int x=0;x<info.xRes;x++) {
+			Point pt = {x,y};
+			PixelValue v;
+			v.grayscale = 255-src[x];
+			imaqSetPixel(output, pt, v);
+		}
+	}
+
+	return (uintptr_t)output;
 }
+
+DLL_EXPORT void CALLCONV compute_com(uintptr_t *imagePtr, float* pos)
+{
+	Image* img = (Image*)imagePtr;
+	
+	ImageInfo info;
+	imaqGetImageInfo(img, &info);
+
+	void* p = imaqGetPixelAddress(img, Point());
+
+	if (info.imageType == IMAQ_IMAGE_U8) {
+		uint8_t *data = (uint8_t*)p;
+
+		for (int y=0;y<info.yRes;y++) {
+			for (int x=0;x<info.xRes;x++) {
+			}
+		}
+	}
+}
+
 
 
 DLL_EXPORT uintptr_t * CALLCONV test_image()
---
+++ cudaqi/GPUImage.cu
@@ -0,0 +1,135 @@
+/*
+GPU Simulation and single-molecule localization for micromirror optics
+Author: Jelmer Cnossen (jcnossen at gmail)
+License: GPL
+*/
+
+#include "GPUBase.h"
+#include "GPUImage.h"
+
+#define NPP_CALL(STMT) {NppStatus _status = STMT; }
+	//if( (_status) != NPP_NO_ERROR) throwError( "NPP error for call %s. Code: %d", #STMT, _status); }
+
+void GPUImage::free()
+{
+	if (d_img) {
+		cudaFree(d_img);
+		d_img=0;
+	}
+	w=h=0;
+}
+
+void GPUImage::init(int w,int h)
+{
+	this->w=w;
+	this->h=h;
+	cudaMalloc(&d_img, w*h*sizeof(float));
+	clear();
+}
+
+void GPUImage::clear()
+{
+	cudaMemset(d_img, 0, sizeof(float)*w*h);
+}
+
+
+float GPUImage::abssum(cudaStream_t s) const {
+	if (s) cublasSetKernelStream(s);
+
+	return cublasSasum(w*h, d_img, 1);
+}
+
+void GPUImage::copyTo(GPUImage& img) const {
+	if (img.w!=w||img.h!=h) 
+		img.resize(w,h);
+	cudaMemcpy(img.d_img, d_img, w*h*sizeof(float), cudaMemcpyDeviceToDevice);
+}
+
+float GPUImage::normalizeSum(float wantedsum, cudaStream_t s)
+{
+	float sum=abssum(s);
+	if (s) nppSetStream(s);
+	NPP_CALL(nppsMulC_32f_I(wantedsum/sum, d_img, w*h));
+	return sum;
+}
+
+void GPUImage::addfloat(float value, cudaStream_t s)
+{
+	if (s) nppSetStream(s);
+	NPP_CALL(nppsAddC_32f_I(value, d_img, w*h));
+}
+
+float GPUImage::normalizeMax(float wantedmax, cudaStream_t s)
+{
+	float m;
+	if (s) nppSetStream(s);
+
+	int maxIndex = cublasIsamax(w*h, d_img, 1) - 1; // cublas uses fortran indexing (1-based)
+	cudaMemcpy(&m, &d_img[maxIndex], sizeof(m), cudaMemcpyDeviceToHost);
+	if (m > 0.0f) {
+		NPP_CALL(nppsMulC_32f_I(wantedmax/m, d_img, w*h));
+	}
+	return m;
+}
+
+
+
+float GPUImage::normalizeMinMax(float wantedmin, float wantedmax, cudaStream_t s)
+{
+	float max,min;
+	if (s) nppSetStream(s);
+	float wanteddif = wantedmax-wantedmin;
+
+	int maxIndex = cublasIsamax(w*h, d_img, 1) - 1; // cublas uses fortran indexing (1-based)
+	int minIndex = cublasIsamin(w*h, d_img, 1) - 1;
+
+	cudaMemcpy(&max, &d_img[maxIndex], sizeof(max), cudaMemcpyDeviceToHost);
+	cudaMemcpy(&min, &d_img[minIndex], sizeof(min), cudaMemcpyDeviceToHost);
+	float dif=max-min;
+	if (dif < 0.0f) { 
+		dif=max; max=min; min=dif; dif=max-min;
+	}
+	if (dif > 0.0f) {
+		NPP_CALL(nppsMulC_32f_I(wanteddif/dif, d_img, w*h));
+		NPP_CALL(nppsAddC_32f_I(wantedmin - min * wanteddif / dif, d_img, w*h));
+	}
+	return dif;
+}
+
+
+__global__ void ClampImage(float* d, float maxVal, int2 size)
+{
+	ITEMIDX();
+	float v = d[idx];
+	if (v > maxVal) v = maxVal;
+	d[idx] = v;
+}
+
+void GPUImage::clamp(float max, cudaStream_t s) {
+	int2 size;
+	size.x = w; size.y = h;
+	ClampImage<<<dim3(w/BLKSIZE, h/BLKSIZE), dim3(BLKSIZE,BLKSIZE),0,s>>>(d_img, max, size);
+}
+
+void GPUImage::resize(int w,int h)
+{
+	if (this->w != w || this->h != h) {
+		free();
+		init(w,h);
+	}
+}
+
+bool GPUImage::haveNaNs()
+{
+	float val = cublasSasum(w*h,d_img,1);
+	return val!=val;
+}
+
+
+void GPUImage::add(const GPUImage& src, cudaStream_t s)
+{
+	if (s) nppSetStream(s);
+	NPP_CALL(nppsAdd_32f_I(src.d_img, d_img, w*h));
+}
+
+
