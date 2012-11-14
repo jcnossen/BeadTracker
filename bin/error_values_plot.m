7bc1df9e47f820e348ab1d0253a94e2031d3ac65
jcnossen <j.cnossen@gmail.com>
2012-11-14 19:06:04 +0100

    benchmark result fix

--- bin/error_values_plot.m
+++ bin/error_values_plot.m
@@ -6,27 +6,44 @@ function error_values_plot(dirname)
         figure(2); scatter_plot('dataset_result/roi400');
     end
     
-    
-
 end
 
 function scatter_plot(dirname)
     truepos = dlmread([dirname '/true-pos.csv']);
     jtrkcom = dlmread([dirname '/jtrk-com.csv']);
     jtrkxcor = dlmread([dirname '/jtrk-xcor.csv']);
+    jtrkqi = dlmread([dirname '/jtrk-qi.csv']);
     lvtrkcom = dlmread([dirname '/lvtrk-com.csv']);
     lvtrkqi = dlmread([dirname '/lvtrk-qi.csv']);
     lvtrkxcor = dlmread([dirname '/lvtrk-xcor.csv']);
+    z_results = dlmread([dirname '/measured-z.csv']);
 
 %    plot ( truepos-jtrkcom );
 
-    subplot(311);
-    scatter(truepos(:,2),jtrkxcor(:,2)-truepos(:,2),'.'); title('C++ XCor Interpolated');
-    subplot(312);
-    scatter(truepos(:,2),lvtrkxcor(:,2)-truepos(:,2)-0.5,'.'); title('LV XCor ( -0.5 offset )');
-    subplot(313);
-    scatter(truepos(:,2),lvtrkqi(:,2)-truepos(:,2),'.'); title('QI');
+    % COM
+    subplot(421);
+    scatter(truepos(:,2),lvtrkcom(:,2)-truepos(:,2),'.'); title('LV COM');
+    subplot(422);
+    scatter(truepos(:,2),jtrkcom(:,2)-truepos(:,2),'.'); title('C++ COM');
 
+    % XCor
+    subplot(423);
+    scatter(truepos(:,2),lvtrkxcor(:,2)-truepos(:,2)-0.5,'.'); title('LV XCor ( -0.5 offset )');
+    subplot(424);
+    scatter(truepos(:,2),jtrkxcor(:,2)-truepos(:,2),'.'); title('C++ XCor');
+
+    % QI
+    subplot(425);
+    scatter(truepos(:,2),lvtrkqi(:,2)-truepos(:,2),'.'); title('LV QI');
+    subplot(426);
+    scatter(truepos(:,2),jtrkqi(:,2)-truepos(:,2),'.'); title('C++ QI');
+
+    % Z
+    subplot(427);
+    scatter(truepos(:,2),z_results(:,2)-truepos(:,3),'.'); title('LV Z');
+    subplot(428);
+    scatter(truepos(:,2),z_results(:,1)-truepos(:,3),'.'); title('C++ Z');
+    
 end
 
 
