a11242fd283ac78d1f14216df041075daf24a703
jelmer <j.p.cnossen@tudelft.nl>
2013-04-23 19:53:15 +0200

    fastcam..

---
+++ fastcamgrab/lv_fastcam.cpp
@@ -0,0 +1,2 @@
+#include "labview.h"
+
---
+++ fastcamgrab/main.cpp
@@ -0,0 +1,99 @@
+#include "std_incl.h"
+#include <Windows.h>
+#include "resource.h"
+#include "fastcmos.h"
+
+const char *WindowClass = "wndcls";
+HWND window;
+
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
+	switch (message)
+	{
+	case WM_COMMAND:{
+		int wmId    = LOWORD(wParam);
+		int wmEvent = HIWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
+		{
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		case ID_CAMERA_OPEN:
+			break;
+		case ID_CAMERA_CLOSE:
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;}
+	case WM_PAINT:{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+
+		EndPaint(hWnd, &ps);
+		break;}
+	case WM_CLOSE:
+		DestroyWindow(hWnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+
+void Init(HINSTANCE instance)
+{
+	WNDCLASSEX wcex;
+	memset(&wcex, 0, sizeof(WNDCLASSEX));
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	const char *WindowClass = "test";
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.hInstance		= instance;
+	wcex.lpszMenuName	= MAKEINTRESOURCE(IDR_MENU);
+	wcex.lpszClassName	= WindowClass;
+
+	ATOM cls = RegisterClassEx(&wcex);
+
+	window = CreateWindow(WindowClass, "Fast grab test", WS_OVERLAPPEDWINDOW, 
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, instance, NULL);
+	
+	ShowWindow(window, SW_SHOW);
+	UpdateWindow(window);
+}
+
+int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPTSTR cmdLine, int nCmdShow)
+{
+	Init(hInst);
+
+	MSG msg;
+
+	// Main message loop:
+	while (1)
+	{
+		while (PeekMessage(&msg, 0, 0, 0, PM_REMOVE)) {
+			if (msg.message == WM_QUIT) {
+				break;
+			}
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		if(msg.message == WM_QUIT)
+			break;
+	}
+
+	return (int) msg.wParam;
+}
+
+
+
