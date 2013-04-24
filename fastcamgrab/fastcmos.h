9f0ff10b9be2737611843c87c3d5d3e21eed30cd
jelmer <j.p.cnossen@tudelft.nl>
2013-04-24 16:17:54 +0200

    * fast mikrotron camera code (image display, but no grabbing yet)

--- fastcamgrab/fastcmos.h
+++ fastcamgrab/fastcmos.h
@@ -3,12 +3,33 @@
 #include "niimaq.h"
 #include "utils.h"
 
+class IMAQBuffer {
+public:
+	IMAQBuffer() { sid=0; data=0; }
+	~IMAQBuffer();
+
+	void *data;
+	SESSION_ID sid;
+};
+
+struct ROI
+{
+	ROI() { x=y=w=h=0; }
+	ROI(int W,int H) { x=y=0; w=W;h=H; }
+	int x,y,w,h;
+};
+
 class FastCMOS
 {
 public:
 
+	enum { MAX_ROI=4 };
+
 	FastCMOS(SESSION_ID session);
 	~FastCMOS();
+
+	// Set ROI first!
+	void setup(int nbuffers);
 
 	void write(const std::string& cmd);
 	std::string read();
@@ -24,17 +45,37 @@ public:
 	int getGain() { return icmd(":D?"); }
 	void setGain(int gain) { cmd(SPrintf(":D%04x")); }
 
-	int getFramerate(); 
-	int getShuttertime();
+	int getLostFrames();
+	int getFramecount();
 
+	struct FramerateInfo { int maxfps, fps; };
+	FramerateInfo getFramerate(); 
+	void setFramerate(int fps);
+	int getShuttertime();
 
 	bool isFramecounterEnabled() { return icmd(":u?") == 1; }
 	void setFramecounter(bool v) { cmd(v ? ":u1" : ":u0" ); }
 
+	ROI setROI(ROI roi); // returns fitted ROI
+	ROI setROI(int nroi, ROI* rois);
+	ROI getROI();
+	ROI getAcqWindow();
+	void *getLastFrame();
+	void start();
+	void stop();
+
+	IMAQBuffer* snap();
+
 protected:
+	ROI roi[MAX_ROI];
+	int numROI, nbuffers;
 	SESSION_ID session;
 	std::string readBuf;
+	BUFLIST_ID buflist;
+	std::vector<void*> buffers;
+	int curBuf;
 
+	void checkIMAQ(Int32 errc);
 };
 
 
--- fastcamgrab/main.cpp
+++ fastcamgrab/main.cpp
@@ -9,20 +9,95 @@ HWND window;
 INTERFACE_ID ifid;
 SESSION_ID session;
 FastCMOS* fastCMOS;
+HDC drawcontext, hdcMem;
+HBITMAP hBitmap;
+double last_update=0;
+double update_freq=10;
 
 void msgbox(const std::string& msg)
 {
-	MessageBox(0, msg.c_str(), "Msg:", MB_OK);
+	MessageBox(0, msg.c_str(), "Msg:", MB_OK|MB_SYSTEMMODAL);
+}
+
+void InitDisplay(int w,int h)
+{
+	drawcontext = GetDC(window);
+
+	BITMAPINFO bmi = { 0 };
+	uchar* data = NULL;
+	bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+	bmi.bmiHeader.biWidth = w;
+	bmi.bmiHeader.biHeight = h;
+	bmi.bmiHeader.biPlanes = 1;
+	bmi.bmiHeader.biBitCount = 24;
+	bmi.bmiHeader.biCompression = BI_RGB;
+	hBitmap = CreateDIBSection(drawcontext, &bmi, DIB_RGB_COLORS, (LPVOID*)&data, NULL, 0);
+
+	hdcMem = CreateCompatibleDC(NULL);
+
+	
+	// Green test circle
+	for (int y=0;y<h;y++) {
+		for (int x=0;x<w;x++) {
+			int r2 = (x-w/2)*(x-w/2) + (y-h/2)*(y-h/2);
+			data[ (y*w+x)*3 + 0 ] = 0;
+			data[ (y*w+x)*3 + 1 ] = r2 < 50*50 ? 255 : 0;
+			data[ (y*w+x)*3 + 2 ] = 0;
+		}
+	}
+	SelectObject(hdcMem, hBitmap);
+
+	BitBlt(GetDC(window), 0, 0, w,h, hdcMem, 0,0,  SRCCOPY);
+}
+
+void CloseDisplay()
+{
+	if (hBitmap) {
+		DeleteDC(hdcMem);
+		DeleteObject(hBitmap);
+		hBitmap = 0;
+	}
 }
 
 void CameraClose()
 {
+	fastCMOS->stop();
+
 	delete fastCMOS;
 	fastCMOS = 0;
 
 	imgClose(session, 0);
 	imgClose(ifid, 1);
+
+	CloseDisplay();
 }
+
+
+void CameraConfigure()
+{
+	CloseDisplay();
+
+	int w = 600, h= 600;
+
+	fastCMOS->setFramerate(10);
+	int fps=  fastCMOS->getFramerate().fps;
+
+	ROI rfit = fastCMOS->setROI(ROI(w,h));
+	ROI roi = fastCMOS->getROI();
+	fastCMOS->setup(50);
+	fastCMOS->start();
+	//IMAQBuffer* buf = fastCMOS->snap();
+
+	int mode = fastCMOS->getMode();
+	
+	InitDisplay(w,h);
+
+//	IMAQBuffer* buf = fastCMOS->snap();
+	
+
+//	void *buf = fastCMOS->getLastFrame();
+}
+
 
 void CameraOpen()
 {
@@ -42,14 +117,12 @@ void CameraOpen()
 
 	fastCMOS = new FastCMOS(session);
 	std::string info = fastCMOS->readInfo();
-	msgbox(info);
 
-	int fps = fastCMOS->getFramerate();
+	CameraConfigure();
+
+	int fps = fastCMOS->getFramerate().fps;
 	dbgprintf("FPS: %d\n", fps);
-
 }
-
-
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -92,6 +165,19 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
+void Update()
+{
+	double t = GetPreciseTime();
+	if (t-last_update > 1.0f/update_freq) {
+
+		if (fastCMOS) {
+			int fc = fastCMOS->getFramecount();
+
+			dbgprintf("Frames: %d\n", fc);
+		}
+		last_update = t;
+	}
+}
 
 void Init(HINSTANCE instance)
 {
@@ -134,6 +220,8 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPTSTR cmdLine, int n
 		}
 		if(msg.message == WM_QUIT)
 			break;
+
+		Update();
 	}
 
 	return (int) msg.wParam;
