be7c9884fed8330f3d987eda66aa4657b9c7dc35
Jelmer (D012R) <j@j.com>
2014-01-27 18:08:16 +0100

    * new binary trace format

--- matlab/qtrk_read_bin_trace.m
+++ matlab/qtrk_read_bin_trace.m
@@ -1,10 +1,12 @@
 function [beadx, beady, beadz, timestamps, frameinfo, axisnames] = qtrk_read_bin_trace(filename, frames, beads, refbead)
 % Read XYZ traces from a binary trace output produced by TweezerTracker
-% [beadx, beady, beadz, timestamps] = qtrk_read_bin_trace(filename, frames, beads, refbead)
+% [beadx, beady, beadz, timestamps, frameinfo, axisnames] =  ...
+%    qtrk_read_bin_trace(filename, frames, beads, refbead)
 %
 % Example: qtrk_read_bin_trace('test.bin', 20:100);  % reads frame 20 to 100 for all beads from test.bin
 %
 % beadx,beady,beadz: XYZ data, beads are mapped to columns, frames are mapped to rows
+% frames: list of frame numbers, or -1 for all frames
 % beads: List of beads (1-based indexing)
 % refbead: Index of reference bead (-1 to disable reference bead subtraction)
     if nargin<4
@@ -23,6 +25,10 @@ function [beadx, beady, beadz, timestamps, frameinfo, axisnames] = qtrk_read_bin
     if nargin<2
         frames=1:f_nframes;
     end
+    
+    if isscalar(frames) && frames < 0
+        frames=1:f_nframes;
+    end
    
     beadx = zeros(length(frames),length(beads));
     beady = beadx;
@@ -32,7 +38,7 @@ function [beadx, beady, beadz, timestamps, frameinfo, axisnames] = qtrk_read_bin
     
     fid = fopen(filename);
 
-    bytesPerFrame = 4 + 8 + f_ninfocol * 4 + nbeads * 3 * 4;
+    bytesPerFrame = 4 + 8 + f_ninfocol * 4 + nbeads * 4 * 4;
     lastFrame=-1;
     for k=1:length(frames)
         
@@ -48,6 +54,7 @@ function [beadx, beady, beadz, timestamps, frameinfo, axisnames] = qtrk_read_bin
         frameinfo(k, :) = fread(fid, [1 f_ninfocol], 'single');
         
         xyz = fread(fid, nbeads * 3, 'single');
+        errorvals = fread(fid, nbeads, 'uint32');
         
 		% Read all beads
         bx = xyz( (0:nbeads-1) * 3 + 1 );
